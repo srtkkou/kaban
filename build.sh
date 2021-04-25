ca5a67becf192edd1316e9ba5b7d9d0f3ff53cc0
srtkkou <srtkkou@outlook.jp>
2021-04-25 15:19:36 +0900

    [Add] Added build script for kaban.

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -u
+
+version=`git log -n 1 --date=short --pretty=format:"%cd-%h"`
+echo "* ver: $version"
+
+echo "* go generate"
+go generate ./...
+
+echo "* go fmt"
+go fmt ./...
+
+echo "* go vet"
+go vet ./...
+
+echo "* staticcheck"
+staticcheck ./...
+
+echo "* go test"
+go test ./...
+
+#echo "* Build kaban: "
+#env GOARCH=amd64 GOOS=linux go build \
+#  -ldflags "-s -X main.version=$version" -race \
+#  -o kaban.exe cmd/kaban/main.go
+#if [ $? -eq 0 ]; then
+#  echo "  OK"
+#else
+#  echo "  NG"
+#fi
