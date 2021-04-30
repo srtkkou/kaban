1f977b11759e6a99b83648c1d7faa1c2e3488451
srtkkou <srtkkou@outlook.jp>
2021-04-30 19:17:42 +0900

    [Update] Added load helpers for slice.

--- load_helper.go
+++ load_helper.go
@@ -1,5 +1,9 @@
 package kaban
 
+import (
+	"time"
+)
+
 func (k *Kaban) Must(v interface{}, err error) {
 	if err != nil {
 		panic(err)
@@ -57,6 +61,26 @@ func (k *Kaban) LoadUint32(key string) (v uint32, err error) {
 }
 
 func (k *Kaban) LoadUint64(key string) (v uint64, err error) {
+	err = k.Load(key, &v)
+	return v, err
+}
+
+func (k *Kaban) LoadTime(key string) (v time.Time, err error) {
+	err = k.Load(key, &v)
+	return v, err
+}
+
+func (k *Kaban) LoadStrings(key string) (v []string, err error) {
+	err = k.Load(key, &v)
+	return v, err
+}
+
+func (k *Kaban) LoadInts(key string) (v []int, err error) {
+	err = k.Load(key, &v)
+	return v, err
+}
+
+func (k *Kaban) LoadUints(key string) (v []uint, err error) {
 	err = k.Load(key, &v)
 	return v, err
 }
